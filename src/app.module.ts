67a0de5b62bd99f5579f2c140ef577c4ef145edd
Alexandr <19Odin95@mail.ru>
2021-07-21 03:05:30 +0400

    feat: integration with GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -9,6 +11,11 @@ console.log(process.env.MONGO_STRING_URL_CONNECTION);
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     MongooseModule.forRoot(process.env.MONGO_STRING_URL_CONNECTION, {
       user: process.env.MONGO_USER_NAME,
       pass: process.env.MONGO_USER_PASS,
